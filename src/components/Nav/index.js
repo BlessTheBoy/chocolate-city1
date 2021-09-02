095bfd9b91736796548d058999bf30a4fb7a4eba
Faruq Bakare <bakarefaruq.a@gmail.com>
2021-09-02 16:20:29 +0100

    nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,28 +1,31 @@
-import "./style.css";
+import "./style.scss";
 import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
+import Container from "../Container";
 
 function Nav() {
   return (
-    <div className="nav">
-      <Link to="/">
-        <img src={logo} alt="" className="nav_logo" />
-      </Link>
+    <Container>
+      <div className="nav">
+        <Link to="/">
+          <img src={logo} alt="" className="nav_logo" />
+        </Link>
 
-      <nav className="nav_nav">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">Contact</Link>
-          </li>
-          <li>
-            <Link to="/users">About</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+        <nav className="nav_nav">
+          <ul>
+            <li className="active">
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </Container>
   );
 }
 
